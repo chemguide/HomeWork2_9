7a95421a9701263d90810f3e7ef4bb2da54f2889
chemguide <argentum198@gmail.com>
2024-07-15 18:58:07 +0500

    Initial commit

---
+++ src/main/java/com/example/hw2_9_stream/Hw29StreamApplication.java
@@ -0,0 +1,13 @@
+package com.example.hw2_9_stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw29StreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw29StreamApplication.class, args);
+	}
+
+}
