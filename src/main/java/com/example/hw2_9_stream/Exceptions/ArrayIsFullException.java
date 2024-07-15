048ad9d9df033ab5e67d9b487397ad6ef608c75d
chemguide <argentum198@gmail.com>
2024-07-15 23:06:37 +0500

    check hw

--- src/main/java/com/example/hw2_9_stream/Exceptions/ArrayIsFullException.java
+++ src/main/java/com/example/hw2_9_stream/Exceptions/ArrayIsFullException.java
@@ -1,5 +1,9 @@
 package com.example.hw2_9_stream.Exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.INSUFFICIENT_STORAGE)
 public class ArrayIsFullException extends RuntimeException {
     public ArrayIsFullException() {
         super();
--- src/main/java/com/example/hw2_9_stream/Exceptions/EmployeeAlreadyAddedException.java
+++ src/main/java/com/example/hw2_9_stream/Exceptions/EmployeeAlreadyAddedException.java
@@ -1,5 +1,9 @@
 package com.example.hw2_9_stream.Exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.ALREADY_REPORTED)
 public class EmployeeAlreadyAddedException extends RuntimeException {
     public EmployeeAlreadyAddedException() {
         super();
